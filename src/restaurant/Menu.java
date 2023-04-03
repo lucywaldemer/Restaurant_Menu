8ff9eb08a9e58e3b4d018e864c192966e15e183e
Lu Waldemer <lucywaldemer@gmail.com>
2023-04-03 18:55:21 -0500

    Initial commit

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,7 @@
+package restaurant;
+import java.util.Date;
+import java.util.ArrayList;
+public class Menu {
+    Date dateAdded;
+    ArrayList items;
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,16 @@
+package restaurant;
+import java.util.Date;
+import java.util.ArrayList;
+public class MenuItem {
+    String description;
+    Double price;
+    String category;
+    Boolean isNew;
+    String name;
+    Boolean isAvailable;
+    Boolean isVegan;
+    Date dateAdded;
+    ArrayList allergens;
+    int spiceLevel;
+
+}
